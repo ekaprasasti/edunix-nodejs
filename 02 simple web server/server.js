3079cfb81dd4c686ad0e4442e53622ddb0d78af7
eka prasasti <ekaputraprasasti@gmail.com>
2016-10-05 13:11:11 +0700

    buat mimeTypes(type file)

--- 02 simple web server/server.js
+++ 02 simple web server/server.js
@@ -3,3 +3,13 @@ var http = require('http');
 var url = require('url');
 var path = require('path');
 var fs = require('fs');
+
+// Array of Mime Types
+var mimeTypes = {
+	"html" : "text/html",
+	"jpeg" : "image/jpeg",
+	"jpg" : "image/jpeg",
+	"png" : "image/png",
+	"js" : "image/javascript",
+	"css" : "text/css"
+}
