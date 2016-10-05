8cb0f00f03a7de7f97d14f8f05d8d41a7faf3c4b
eka prasasti <ekaputraprasasti@gmail.com>
2016-10-05 20:31:20 +0700

    create transporter.sendMail() for try and catch, tester email: success if login in email account

--- 03 basic express server/express_website/routes/contact.js
+++ 03 basic express server/express_website/routes/contact.js
@@ -16,15 +16,24 @@ router.post('/send', function(req, res, next) {
 		}
 	});
 
-	var mainOptions = {
+	var mailOptions = {
 		from: 'John Doe <johndoe@outlook.com>',
-		to: 'ekaputraprasasti@gmail.com',
+		to: 'dummy.analytic@gmail.com',
 		subject: 'Website Submission',
 		text: 'You have a new submission with the following details...Name: '+req.body.name+' Email: '+req.body.email+ 'Message: '+req.body.message, 
 		html: '<p>You got a new submission with the following details..</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
 	};
 
-	
+	transporter.sendMail(mailOptions, function(error, info){
+		if (error) {
+			console.log(error);
+			res.redirect('/');
+		}
+		else {
+			console.log('Message sent: '+info.response);
+			res.redirect('/');
+		}
+	});
 });
 
 module.exports = router;
