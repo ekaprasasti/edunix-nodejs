6b0c2820c0c8af32e1bdcbffcb91401f1befb3d1
eka prasasti <ekaputraprasasti@gmail.com>
2016-10-06 08:29:56 +0700

    (bug fix) load module express validator, change middleware app.use() in multer

--- 04 user login system/nodeaouth/app.js
+++ 04 user login system/nodeaouth/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
@@ -24,7 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // Handle file upload
-app.use(multer({dest:'./uploads'}));
+var upload = multer({dest:'./uploads'});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
