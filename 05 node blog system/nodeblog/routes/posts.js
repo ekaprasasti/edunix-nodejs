4cfc463523a2cd4e32dfe14da4dd1c5a80e82b9b
eka prasasti <ekaputraprasasti@gmail.com>
2016-10-11 19:29:24 +0700

    (5.5) check image file info & set default image

--- 05 node blog system/nodeblog/routes/posts.js
+++ 05 node blog system/nodeblog/routes/posts.js
@@ -9,13 +9,30 @@ router.get('/add', function(req, res, next){
 	});
 });
 
-router.post('/add', function(req, res, next){
+router.post('/add', upload.single('mainimage'), function(req, res, next){
 	// Get form values
 	var title 		= req.body.title;
 	var category 	= req.body.category;
 	var body 		= req.body.body;
 	var author 		= req.body.author;
 	var date 		= new Date();
+
+	// Check for image field
+	if (req.file) {
+		console.log('Uploading File...');
+
+		// File info
+		var profileImageOriginalName 	= req.file.originalname;
+		var profileImageName 			= req.file.name;
+		var profileImageMime			= req.file.mimetype;
+		var profileImagePath			= req.file.path;
+		var profileImageExt				= req.file.extension;
+		var profileImageSize			= req.file.size;
+	}
+	else {
+		// Set default image
+		var profileImageName = 'noimage.png';
+	}
 });
 
 module.exports = router;
