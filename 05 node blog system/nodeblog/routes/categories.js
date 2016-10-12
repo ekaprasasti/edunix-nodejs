d96d8899dd7d76451d7c5759e54eea18a0680d31
eka prasasti <ekaputraprasasti@gmail.com>
2016-10-12 13:10:23 +0700

    (5.6) create file categories.js in /route, create routes in app.js, create form input categories in addcategories.jade

---
+++ 05 node blog system/nodeblog/routes/categories.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+
+
+router.get('/add', function(req, res, next) {
+  res.render('addcategory', {
+  	"title": "Add Category"
+  })
+});
+
+module.exports = router;
